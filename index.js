9835b0a4acb17f1d16980d39cc17254319803249
Jonas Lima de Amorim <dev.amorim@outlook.com>
2023-09-26 21:52:09 -0300

    Padronização de nomenclaturas

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get("/polls", async function (req, res) {
     if (response) {
       res.json(response.Items);
     } else {
-      res.status(404).json({ error: "Could not find polls in the database" });
+      res.status(404).json({ message: "Could not find polls in the database" });
     }
   } catch (error) {
     console.log(error);
@@ -60,11 +60,11 @@ app.get("/polls/:pollId", async function (req, res) {
     } else {
       res
         .status(404)
-        .json({ error: 'Could not find poll with provided "pollId"' });
+        .json({ message: 'Could not find poll with provided "pollId"' });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Could not retreive poll" });
+    res.status(500).json({ message: "Could not retreive poll", error: error });
   }
 });
 
@@ -72,9 +72,9 @@ app.get("/polls/:pollId", async function (req, res) {
 app.post("/polls", async function (req, res) {
   const { question, options } = req.body;
   if (typeof question !== "string") {
-    res.status(400).json({ error: '"question" must be a string' });
+    res.status(400).json({ message: '"question" must be a string' });
   } else if (typeof options !== "object") {
-    res.status(400).json({ error: '"options" must be a array of strings' });
+    res.status(400).json({ message: '"options" must be a array of strings' });
   }
 
   const pollId = uuidv4();
@@ -95,7 +95,7 @@ app.post("/polls", async function (req, res) {
     res.json({ pollId, question, enabled: true, options: databaseOptions });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Could not create poll", error: error });
+    res.status(500).json({ message: "Could not create poll", error });
   }
 });
 
@@ -103,9 +103,9 @@ app.post("/polls", async function (req, res) {
 app.post("/votes", async function (req, res) {
   const { pollId, label } = req.body;
   if (typeof pollId !== "string") {
-    res.status(400).json({ error: '"pollId" must be a string' });
+    res.status(400).json({ message: '"pollId" must be a string' });
   } else if (typeof label !== "string") {
-    res.status(400).json({ error: '"label" must be a string' });
+    res.status(400).json({ message: '"label" must be a string' });
   }
 
   let params = {
@@ -121,7 +121,7 @@ app.post("/votes", async function (req, res) {
     if (!Item) {
       return res
         .status(404)
-        .json({ error: "Could not find the provided pollId" });
+        .json({ message: "Could not find the provided pollId" });
     }
 
     let options = Item.options;
@@ -153,7 +153,7 @@ app.post("/votes", async function (req, res) {
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    message: "Not Found",
   });
 });
 
